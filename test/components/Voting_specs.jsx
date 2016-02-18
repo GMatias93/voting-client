a99c9a03e1b8fbb74160ac499bd5d58be60f624b
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-18 10:53:17 -0800

    test: added test for Voting component (not passing)

--- test/components/Voting_specs.jsx
+++ test/components/Voting_specs.jsx
@@ -1,5 +1,20 @@
+import React from 'react/addons';
 import Voting from '../../src/components/voting';
+import { expect } from 'chai';
+
+const { renderIntoDocument, scryRenderedDOMComponentsWithTag } = React.addons.TestUtils;
 
 describe('Voting', () => {
+
+    it('renders a pair of buttons', () => {
+        const component = renderIntoDocument(
+            <Voting pair={["The Motto", "You and The 6"]} />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].textContent).to.equal("The Motto");
+        expect(buttons[1].textContent).to.equal("You and The 6");
+    });
 
 });
