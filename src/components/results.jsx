1e09683694e421442e195a0a9117cfb23fcd1a45
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-18 20:36:12 -0800

    feat: moved tally funcitonality into it's component

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './winner';
+import Tally from './tally';
 
 export default class Results extends Component {
     constructor(props) {
@@ -9,32 +10,12 @@ export default class Results extends Component {
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
 
-    getPair() {
-        return this.props.pair || [];
-    }
-
-    getVotes(entry) {
-        if(this.props.tally && this.props.tally.has(entry)) {
-            return this.props.tally.get(entry);
-        }
-        return 0;
-    }
-
     render() {
         return (
             this.props.winner ?
             <Winner ref="winner" winner={this.props.winner} /> :
             <div className="results">
-                <div className="tally">
-                    {this.getPair().map(entry =>
-                        <div key={entry} className="entry">
-                            <h1>{entry}</h1>
-                            <div className="voteCount">
-                                {this.getVotes(entry)}
-                            </div>
-                        </div>
-                    )}
-                </div>
+                <Tally pair={this.props.pair} tally={this.props.tally} />
                 <div className="management">
                     <button ref="next"
                             className="next"
