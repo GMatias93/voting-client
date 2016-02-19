6ebf769c2a7117ecbca8b7f361ce2f6887b68fd3
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-18 19:07:42 -0800

    feat: added results route and tally for results component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 const pair = List.of('Change Location', 'Plastic Bag');
+const tally = Map({'Change Location': 43423, 'Plastic Bag': 4});
 
 export default class App extends Component {
     constructor(props) {
         super(props);
 
     }
+
     render() {
-        return React.cloneElement(this.props.children, {pair: pair});
+        return React.cloneElement(this.props.children, {
+            pair: pair,
+            tally: tally
+        });
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/app';
 import Voting from './components/voting';
+import Results from './components/results';
 
 const routes = <Route component={App}>
+    <Route path="/results" component={Results} />
     <Route path="/" component={Voting} />
     </Route>;
 
