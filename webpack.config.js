ecc65ba34b7522bcdb9110cb03be533365e8ecbe
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-22 12:30:13 -0800

    style: finished styling

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,10 @@ module.exports = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
-    },
-  ],
+    }, {
+        test:/\.css$/,
+        loader:'style!css!autoprefixer?browsers=last 2 versions'
+    }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
