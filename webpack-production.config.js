647f8d882fe1b7a026c5689d7603b08df8b200b2
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-23 13:46:13 -0800

    feat: getting app ready for production

---
+++ webpack-production.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/index.jsx'
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel',
+                include: path.join(__diname, 'src')
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css!autoprefixer?browsers=last 2 versions',
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    }
+};
