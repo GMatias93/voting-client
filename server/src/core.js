8f3217271f50117c32ef7f27b4cc9137d0429b4c
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-24 10:36:37 -0800

    feat: added server code to client app

---
+++ server/src/core.js
@@ -0,0 +1,54 @@
+import { List, Map } from 'immutable';
+
+export function setEntries(state, entries) {
+  // const immutableEntries = fromJS(entries, (key, value) => {
+  //   return value.toList();
+  // });
+  // return state.set('entries', immutableEntries);
+
+  return state.set('entries', List(entries));
+}
+
+function getWinners(vote) {
+  if (!vote) {
+    return [];
+  }
+
+  const [a, b] = vote.get('pair');
+  const aVote = vote.getIn(['tally', a], 0);
+  const bVote = vote.getIn(['tally', b], 0);
+
+  if (aVote > bVote) {
+    return [a];
+  } else if (bVote > aVote) {
+    return [b];
+  } else {
+    return [a, b];
+  }
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                        .concat(getWinners(state.get('vote')));
+
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({ pair: entries.take(2) }),
+      entries: entries.skip(2)
+    });
+  }
+}
+
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
+    0,
+    tally => tally + 1
+  );
+}
+
+export const INITIAL_STATE = Map();
