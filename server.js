b4e6bf66105693b49549d45bc9e8953f7f419b9b
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-27 11:43:45 -0800

    fix: fixed reference error that has been keeping up at night for a week

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import socket from 'socket.io';
-import config from './webpack.config';
-import webpack from 'webpack';
 import express from 'express';
 import path from 'path';
 const app = express();
 const port = process.env.PORT || 8300;
 
-const static_path = path.join(__dirname, '/client');
+const static_path = path.join('.', __dirname, 'client');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,18 +12,18 @@ app.use((req, res, next) => {
     next();
 });
 
-const server = app.listen(port, () => {
-    const host = server.address().address;
-    console.log(`Server listening at port ${port}`);
-});
-
 app.use(express.static(static_path))
     .get('/', (req, res) => {
         res.sendFile('index.html', {
-            root: './client'
+            root: static_path
         });
     });
 
+const server = app.listen(port, () => {
+    const host = server.address().address;
+    console.log(`Server listening at port ${port}`);
+    console.log(__dirname);
+});
 
 export default function startServer(store) {
     const io = socket(server);
