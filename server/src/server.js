40226ecc8b7e41744e91966a5642d0727210f5ba
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-29 14:11:41 -0800

    fix: moved all server code into server.js in server directory. getting new error

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,13 @@
-// import Server from 'socket.io';
-import io from '../../server.js';
+import socket from 'socket.io';
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 8300;
 
 export default function startServer(store) {
-    // const io = new Server().attach(process.env.PORT || 8300);
+    const io = socket.listen(server);
 
     store.subscribe(
         () => io.emit('state', store.getState().toJS())
@@ -12,5 +17,6 @@ export default function startServer(store) {
         socket.emit('state', store.getState().toJS());
         socket.on('action', store.dispatch.bind(store));
     });
-
 }
+
+server.listen(port);
