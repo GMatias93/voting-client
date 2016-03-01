7c6a914538637a9d2ef0b8390f1fd788e5c200fb
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-01 10:31:41 -0800

    fix: added json-loader issue fixed

--- server/src/core.js
+++ server/src/core.js
@@ -1,7 +1,6 @@
 import { List, Map } from 'immutable';
 
 export function setEntries(state, entries) {
-    console.log(`this is entries in core.js line 4: ${entries}`);
   // const immutableEntries = fromJS(entries, (key, value) => {
   //   return value.toList();
   // });
