ccc8736f447a76df179872f125b1ca17f414bc79
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-01 08:50:00 -0800

    fix: hunting down immutable error with json

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 import makeStore from './src/store';
 import startServer from './src/server.js';
-import candidates from './entries';
 
 export const store = makeStore();
 startServer(store);
 
-console.log(`this is state in index.js line 8: ${candidates}`);
-
 store.dispatch({
   type: 'SET_ENTRIES',
-    entries: ['hello', 'tga', 'the blacker the berry']
+    entries: require('./entries.json')
 });
 store.dispatch({ type: 'NEXT' });
