b4449bd3ea809e59dac7607ab6eab17511b15ada
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-02 13:02:11 -0800

    feat: implemented rounds in order to prevent users from getting stuck in final round

--- client/src/containers/voting.jsx
+++ client/src/containers/voting.jsx
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { connect } from 'react-redux';
+import {
+  connect
+} from 'react-redux';
 import Winner from '../components/winner.jsx';
 import Vote from '../components/vote.jsx';
 import * as actionCreators from '../action_creators';
 
 export const Voting = class extends Component {
     constructor(props) {
-        super(props);
+      super(props);
 
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+      this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
 
     render() {
-        return (
-            <div>
-              {this.props.winner ?
-                  <Winner ref="winner" winner={this.props.winner} /> :
-                  <Vote {...this.props} />}
-            </div>
+      return ( < div > {
+          this.props.winner ?
+          < Winner ref = "winner"
+          winner = {
+            this.props.winner
+          }
+          /> : < Vote {...this.props
+          }
+          />} < /div>
         );
+      }
     }
-}
 
-function mapStateToProps(state) {
-    return {
+    function mapStateToProps(state) {
+      return {
         pair: state.getIn(['vote', 'pair']),
-        hasVoted: state.get('hasVoted'),
+        hasVoted: state.getIn(['myVote', 'entry']),
         winner: state.get('winner')
-    };
-}
+      };
+    }
 
-export const VotingContainer = connect(
-    mapStateToProps,
-    actionCreators
-)(Voting);
-
+    export const VotingContainer = connect(
+      mapStateToProps,
+      actionCreators
+    )(Voting);
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,34 +1,41 @@
-import { List, Map } from 'immutable';
+import {
+  List,
+  Map
+} from 'immutable';
 
 function setState(state, newState) {
-    return state.merge(newState);
+  return state.merge(newState);
 }
 
 function vote(state, entry) {
-    const currentPair = state.getIn(['vote', 'pair']);
-    if(currentPair && currentPair.includes(entry)) {
-        return state.set('hasVoted', entry);
-    }
-    return state;
+	const currentRound = state.getIn(['vote', 'round']);
+  const currentPair = state.getIn(['vote', 'pair']);
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('myVote', Map({
+			round: currentRound,
+			entry
+		}));
+  }
+  return state;
 }
 
 function resetVote(state) {
-    const hasVoted = state.get('hasVoted');
-    const currentPair = state.getIn(['vote', 'pair'], List());
-    if(hasVoted && !currentPair.includes(hasVoted)) {
-        return state.remove('hasVoted');
-    } else {
-        return state;
-    }
+  const votedForRound = state.getIn(['myVote', 'round']);
+  const currentRound = state.getIn(['vote', 'round']);
+  if (votedForRound !== currentRound) {
+    return state.remove('myVote');
+  } else {
+    return state;
+  }
 }
 
 export default function(state = Map(), action) {
-    switch (action.type) {
+  switch (action.type) {
     case 'SET_STATE':
-        return resetVote(setState(state, action.state));
+      return resetVote(setState(state, action.state));
     case 'VOTE':
-        return vote(state, action.entry);
-    }
+      return vote(state, action.entry);
+  }
 
-    return state;
+  return state;
 }
