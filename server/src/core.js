2022b8ea095075f916f39aeba937c54f66d8a454
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-02 10:48:54 -0800

    feat: implementing proper vote reset feature

--- server/src/core.js
+++ server/src/core.js
@@ -1,51 +1,55 @@
-import { List, Map } from 'immutable';
+import {
+    List,
+    Map
+} from 'immutable';
 
 export function setEntries(state, entries) {
-  // const immutableEntries = fromJS(entries, (key, value) => {
-  //   return value.toList();
-  // });
-  // return state.set('entries', immutableEntries);
+    // const immutableEntries = fromJS(entries, (key, value) => {
+    //   return value.toList();
+    // });
+    // return state.set('entries', immutableEntries);
 
-  return state.set('entries', List(entries));
+    return state.set('entries', List(entries));
 }
 
 function getWinners(vote) {
-  if (!vote) {
-    return [];
-  }
+    if (!vote) {
+        return [];
+    }
 
-  const [a, b] = vote.get('pair');
-  const aVote = vote.getIn(['tally', a], 0);
-  const bVote = vote.getIn(['tally', b], 0);
+    const [a, b] = vote.get('pair');
+    const aVote = vote.getIn(['tally', a], 0);
+    const bVote = vote.getIn(['tally', b], 0);
 
-  if (aVote > bVote) {
-    return [a];
-  } else if (bVote > aVote) {
-    return [b];
-  } else {
-    return [a, b];
-  }
+    if (aVote > bVote) {
+		    return [a];
+    } else if (bVote > aVote) {
+        return [b];
+    } else {
+        return [a, b];
+    }
 }
 
 export function next(state) {
-  const entries = state.get('entries')
-                        .concat(getWinners(state.get('vote')));
+    const entries = state.get('entries')
+        .concat(getWinners(state.get('vote')));
 
-  if (entries.size === 1) {
-    return state.remove('vote')
-                .remove('entries')
-                .set('winner', entries.first());
-  } else {
-    return state.merge({
-      vote: Map({ pair: entries.take(2) }),
-      entries: entries.skip(2)
-    });
-  }
+    if (entries.size === 1) {
+        return state.remove('vote')
+            .remove('entries')
+            .set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({
+                pair: entries.take(2)
+            }),
+            entries: entries.skip(2)
+        });
+    }
 }
 
 export function vote(voteState, entry) {
-    //testtest
-    if(voteState.get('pair').includes(entry)) {
+    if (voteState.get('pair').includes(entry)) {
         return voteState.updateIn(
             ['tally', entry],
             0,
