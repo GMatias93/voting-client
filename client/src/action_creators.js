cd9c78dd209898d3430f5ed26b8a7aacf76fb21d
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-02 14:12:04 -0800

    feat: added getClientId action creator

--- client/src/action_creators.js
+++ client/src/action_creators.js
@@ -1,21 +1,32 @@
+export function getClientId(clientId) {
+  return {
+    type: 'GET_CLIENT_ID',
+		clientId
+  };
+}
+
 export function setState(state) {
-    return {
-        type: 'SET_STATE',
-        state
-    };
+  return {
+    type: 'SET_STATE',
+    state
+  };
 }
 
 export function vote(entry) {
-    return {
-        meta: { remote: true },
-        type: 'VOTE',
-        entry
-    };
+  return {
+    meta: {
+      remote: true
+    },
+    type: 'VOTE',
+    entry
+  };
 }
 
 export function next() {
-    return {
-        meta: { remote: true },
-        type: 'NEXT'
-    };
+  return {
+    meta: {
+      remote: true
+    },
+    type: 'NEXT'
+  };
 }
