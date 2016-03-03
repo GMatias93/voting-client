47d2a03a9cc6d265257f83e90047eef2c2be4d41
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-03-02 16:38:11 -0800

    feat: implemented restart functionality on client

--- client/src/containers/results.jsx
+++ client/src/containers/results.jsx
@@ -1,44 +1,59 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { connect } from 'react-redux';
+import {
+  connect
+} from 'react-redux';
 import Winner from '../components/winner';
 import Tally from '../components/tally';
 import * as actionCreators from '../action_creators';
 
 export const Results = class extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-    }
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
 
-    render() {
-        return (
-            this.props.winner ?
-            <Winner ref="winner" winner={this.props.winner} /> :
-            <div className="results">
-                <Tally pair={this.props.pair} tally={this.props.tally} />
-                <div className="management">
-                    <button ref="next"
-                            className="next"
-                            onClick={this.props.next}>
-                        Next
-                    </button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      this.props.winner ?
+      < Winner ref = "winner"
+      winner = {
+        this.props.winner
+      }
+      /> : < div className = "results" >
+      < Tally pair = {
+        this.props.pair
+      }
+      tally = {
+        this.props.tally
+      }
+      /> < div className = "management" >
+      < button ref = "restart"
+      onClick = {
+        this.props.restart
+      } >
+      Restart < /button> < button ref = "next"
+      className = "next"
+      onClick = {
+        this.props.next
+      } >
+      Next < /button> < /div> < /div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        pair: state.getIn(['vote', 'pair']),
-        tally: state.getIn(['vote', 'tally']),
-        winner: state.get('winner')
-    };
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  };
 }
 
 export const ResultsContainer = connect(
-    mapStateToProps,
-    actionCreators
+  mapStateToProps,
+  actionCreators
 )(Results);
--- client/test/components/results_spec.jsx
+++ client/test/components/results_spec.jsx
@@ -1,55 +1,98 @@
 import React from 'react/addons';
 import ReactDOM from 'react-dom';
-import { List, Map } from 'immutable';
-import { Results } from '../../src/containers/results.jsx';
-import { expect } from 'chai';
+import {
+  List,
+  Map
+} from 'immutable';
+import {
+  Results
+} from '../../src/containers/results.jsx';
+import {
+  expect
+} from 'chai';
 
-const { renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate } = React.addons.TestUtils;
+const {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass,
+  Simulate
+} = React.addons.TestUtils;
 
 describe('Results', () => {
 
-    it('renders entries with vote counts or zero', () => {
-        const pair = List.of('Find Your Love', 'From Time');
-        const tally = Map({'From Time': 9});
-        const component = renderIntoDocument(
-            <Results pair={pair} tally={tally} />
-        );
-        const entries = scryRenderedDOMComponentsWithClass(component, 'entry');
-        const [TML, NWTS] = entries.map(e => e.textContent);
-
-        expect(entries.length).to.equal(2);
-        expect(TML).to.contain('Find Your Love');
-        expect(TML).to.contain('0');
-        expect(NWTS).to.contain('From Time');
-        expect(NWTS).to.contain('9');
+  it('renders entries with vote counts or zero', () => {
+    const pair = List.of('Find Your Love', 'From Time');
+    const tally = Map({
+      'From Time': 9
     });
+    const component = renderIntoDocument( < Results pair = {
+        pair
+      }
+      tally = {
+        tally
+      }
+      />
+    );
+    const entries = scryRenderedDOMComponentsWithClass(component, 'entry');
+    const [TML, NWTS] = entries.map(e => e.textContent);
 
-    it('invokes the next callback when the next button is clicked', () => {
-        let nextInvoked = true;
-        const next = () => nextInvoked = true;
+    expect(entries.length).to.equal(2);
+    expect(TML).to.contain('Find Your Love');
+    expect(TML).to.contain('0');
+    expect(NWTS).to.contain('From Time');
+    expect(NWTS).to.contain('9');
+  });
 
-        const pair = List.of('Tuscan Leather', 'Pound Cake');
-        const component = renderIntoDocument(
-            <Results pair={pair}
-            tally={Map()}
-            next={next} />
-        );
-        Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+  it('invokes the next callback when the next button is clicked', () => {
+    let nextInvoked = true;
+    const next = () => nextInvoked = true;
 
-        expect(nextInvoked).to.equal(true);
-    });
+    const pair = List.of('Tuscan Leather', 'Pound Cake');
+    const component = renderIntoDocument( < Results pair = {
+        pair
+      }
+      tally = {
+        Map()
+      }
+      next = {
+        next
+      }
+      />
+    );
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
 
-    it('it should render a winner when there is one', () => {
-        const component = renderIntoDocument(
-            <Results winner={'Connect'}
-            pair={List.of('Connect', 'The Language')}
-            tally={Map()} />
-        );
-        const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(nextInvoked).to.equal(true);
+  });
 
-        expect(winner).to.be.ok;
-        expect(winner.textContent).to.contain('Connect');
+	it('invokes action callback when restart button is clicked', () => {
+		let restartInvoked = false;
+		const pair = List.of('FML', 'Real Friends');
+		const component = renderIntoDocument(
+			<Results pair={pair}
+							 tally={Map()}
+							 restart={() => restartInvoked = true}/>
+				);
+		Simulate.click(ReactDOM.findDOMNode(component.refs.restart));
 
-    });
+		expect(restartInvoked).to.equal(true);
+	});
+
+  it('it should render a winner when there is one', () => {
+    const component = renderIntoDocument( < Results winner = {
+        'Connect'
+      }
+      pair = {
+        List.of('Connect', 'The Language')
+      }
+      tally = {
+        Map()
+      }
+      />
+    );
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Connect');
+
+  });
 
 });
